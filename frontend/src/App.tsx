781492338455d051b25657784399eb61733ee4bc
Arthur Kudabaev <arturo.kudabaev@mail.ru>
2023-04-17 01:19:13 +0500

    Lesson-04

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import Home from "./components/home";
+import {Routes, Route} from "react-router-dom";
+import LoginPage from "./components/auth/login";
+
 
 function App() {
   return (
     <div className="App">
-      <Home />
+        <Routes>
+           <Route path='/' element={<Home />}/>
+            <Route path='login' element={<LoginPage />} />
+        </Routes>
     </div>
   );
 }

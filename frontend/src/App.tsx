d1fe1f0ad5519d4b578a87253d60c057a32a6b12
Arthur Kudabaev <arturo.kudabaev@mail.ru>
2023-04-17 19:37:14 +0500

    Lesson-08

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import './App.css';
 import Home from "./components/home";
 import {Routes, Route} from "react-router-dom";
-import LoginPage from "./components/auth/login";
 import PrivateRoute from "./utils/router/PrivateRoute";
+import AuthRootComponent from "./components/auth";
 
 
 function App() {
@@ -13,7 +12,8 @@ function App() {
             <Route element={<PrivateRoute />}>
                 <Route path='/' element={<Home />}/>
             </Route>
-            <Route path='login' element={<LoginPage />} />
+            <Route path='login' element={<AuthRootComponent />} />
+            <Route path='register' element={<AuthRootComponent />} />
         </Routes>
     </div>
   );
--- frontend/src/components/auth/login/index.tsx
+++ frontend/src/components/auth/login/index.tsx
@@ -1,13 +1,9 @@
 import React from "react";
-import style from './style.module.css';
-import Greeting from "./Greeting";
 
 const LoginPage = () =>{
-    const name = 'Arturo';
     return (
         <div>
-            <h1 className={style.heading}>Login Page</h1>
-            <Greeting name={name} />
+            <h1>Login Page</h1>
         </div>
     )
 }

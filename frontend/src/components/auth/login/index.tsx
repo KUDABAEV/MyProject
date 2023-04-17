ab4042543140af5e24bdff3c6e350556e2bdf59d
Arthur Kudabaev <arturo.kudabaev@mail.ru>
2023-04-17 17:26:40 +0500

    Lesson-06

--- frontend/src/components/auth/login/index.tsx
+++ frontend/src/components/auth/login/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import style from './style.module.css';
 
 const LoginPage = () =>{
     return (
         <div>
-            <h1>Login Page</h1>
+            <h1 className={style.heading}>Login Page</h1>
         </div>
     )
 }
--- frontend/src/utils/router/PrivateRoute.tsx
+++ frontend/src/utils/router/PrivateRoute.tsx
@@ -2,7 +2,7 @@ import {Outlet, Navigate} from "react-router-dom";
 
 const PrivateRoute = () =>{
 
-    const auth = false;
+    const auth = true;
 
     return (
        auth ? <Outlet /> : <Navigate to='login' />
